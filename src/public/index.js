de57bccf54c601c29772c64b6cb5ac42be340aa5
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2020-04-05 22:51:16 +0900

    add color palette

--- src/public/index.js
+++ src/public/index.js
@@ -2,7 +2,7 @@ const ctx = document.getElementById('chart').getContext('2d')
 
 const labels = []
 
-const data = [1, 2, 3, 4, 5]
+let chart
 
 function createLabels(beginDate, endDate) {
   const eDate = new Date(endDate)
@@ -20,11 +20,11 @@ function createLabels(beginDate, endDate) {
   return labels
 }
 
-function createDatasets(pulls, repos, labels) {
+function createDatasets(pulls, repos, labels, bgColors) {
   let datasets = []
-  repos.forEach(repo => {
-    const data = labels.map(label => {
-      return pulls.filter(pull => {
+  repos.forEach((repo, index) => {
+    const data = labels.map((label) => {
+      return pulls.filter((pull) => {
         const createdYearMonth =
           pull.createdAt.split('-')[0] + pull.createdAt.split('-')[1]
         return repo === pull.repo && label === createdYearMonth
@@ -33,9 +33,7 @@ function createDatasets(pulls, repos, labels) {
     datasets.push({
       label: repo,
       data: data,
-      backgroundColor: `rgb(
-        ${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)}
-      )`
+      backgroundColor: bgColors[index],
     })
   })
   return datasets
@@ -45,38 +43,75 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(255))
 }
 
+function getRandomColorCode() {
+  return (
+    '#' +
+    getRandomInt(255).toString(16).padStart(2, '0') +
+    getRandomInt(255).toString(16).padStart(2, '0') +
+    getRandomInt(255).toString(16).padStart(2, '0')
+  )
+}
+
+function createColorPaletteElements(bgColors) {
+  const root = document.querySelector('.color-input')
+  root.innerHTML = ''
+  bgColors.forEach((color) => {
+    root.innerHTML += `<input type="color" value="${color}" />`
+  })
+}
+
+function updateColors() {
+  const newBgColors = Array.from(
+    document.querySelector('.color-input').children
+  ).map((elm) => elm.value)
+  console.log(newBgColors)
+  chart.config.data.datasets = chart.config.data.datasets.map((dataset, index) => ({
+    ...dataset,
+    backgroundColor: newBgColors[index],
+  }))
+  chart.update()
+}
+
 axios
   .all([
     axios.get('http://localhost:1234/config'),
-    axios.get('http://localhost:1234/pulls')
+    axios.get('http://localhost:1234/pulls'),
   ])
   .then(
     axios.spread((config, pulls) => {
       console.log(config, pulls)
       const labels = createLabels(config.data.beginDate, config.data.endDate)
-      const datasets = createDatasets(pulls.data, config.data.repos, labels)
-      console.log(datasets)
+      const bgColors = config.data.repos.map((repo) => getRandomColorCode())
+      console.log(bgColors)
+      const datasets = createDatasets(
+        pulls.data,
+        config.data.repos,
+        labels,
+        bgColors
+      )
       const options = {
         scales: {
           yAxes: [
             {
-              stacked: true
-            }
+              stacked: true,
+            },
           ],
           xAxes: [
             {
-              stacked: true
-            }
-          ]
-        }
+              stacked: true,
+            },
+          ],
+        },
       }
-      const chart = new Chart(ctx, {
+      chart = new Chart(ctx, {
         type: 'bar',
         data: {
           labels: labels,
-          datasets: datasets
+          datasets: datasets,
         },
-        options: options
+        options: options,
       })
+
+      createColorPaletteElements(bgColors)
     })
   )
